907f229f710354c0b4a7fc46b905640e09e788ff
LilCodes <valeibacacheb@gmail.com>
2021-12-01 17:44:15 -0300

    Introduciendo todo lo desarrollado hasta el momento (1/12)

---
+++ app/app.py
@@ -0,0 +1,34 @@
+from os import name
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    nick = request.form.get("nick")
+    return render_template("Login.html", nick=nick)
+
+@app.route('/Ingresar')
+def ingresar():
+    return render_template('Ingresar.html')
+
+@app.route('/Crear')
+def crear():
+    return render_template('Crear.html')
+
+"""
+@app.route('/Crear.html')
+def game():
+    return render_template('Admin.html')
+
+@app.route('/Ingresar.html')
+def game():
+    return render_template('Pupilo.html')
+"""
+
+if __name__ == "__main__":
+    app.run(port = 3000, debug = True)
