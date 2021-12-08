3d48a723f87368433b4a550fbbc4599fc95e47d3
LilCodes <valeibacacheb@gmail.com>
2021-12-07 22:57:10 -0300

    Co-authored-by: Miguel Segovia <Miikkkkk@users.noreply.github.com>

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from os import name
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app=Flask(__name__)
 
@@ -7,29 +7,57 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
-    nick = request.form.get("nick")
-    return render_template("Login.html", nick=nick)
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
 
+@app.route('/main')
+def success():
+    return render_template("Login.html")
+
+#-----------
 @app.route('/Crear')
 def crear():
-    name_sala = request.form.get("name_sala")
-    contrasenia = request.form.get("contrasenia")
-    return render_template('Crear.html', name_sala=name_sala, contrasenia=contrasenia)
+    return render_template('Crear.html')
 
+@app.route('/Crear1', methods=['POST','GET'])
+def crear1():
+    if request.method == "GET":
+        name = request.form["name_sala"]
+        password = request.form["contrasenia"]
+        return redirect(url_for('tutor', name_sala = name))
+    else:
+        name = request.form.get("name_sala")
+        password = request.form.get("contrasenia")
+        return redirect(url_for('tutor', name_sala = name))
+
+@app.route('/Tutor/<name_sala>')
+def tutor(name_sala):
+    return render_template('Tutor.html')
+
+#---------------
 @app.route('/Ingresar')
 def ingresar():
-    contrasenia = request.form.get("contrasenia")
-    return render_template('Ingresar.html', contrasenia=contrasenia)
+    return render_template('Ingresar.html')
 
-@app.route('/Tutor')
-def tutor():
-    return render_template('Tutor.html')
+@app.route('/Ingresar1', methods=['POST','GET'])
+def ingresar1():
+    if request.method == "GET":
+        password1 = request.form["contrasena"]
+        return redirect(url_for('pupilo'))
+    else:
+        password1 = request.form.get("contrasena")
+        return redirect(url_for('pupilo'))
 
 @app.route('/Pupilo')
 def pupilo():
     return render_template('Pupilo.html')
 
+#---------------
 if __name__ == "__main__":
     app.run(port = 3000, debug = True)
