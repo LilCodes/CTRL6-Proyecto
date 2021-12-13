229291c3e4713de996ab4f5ec46d1a9ec2c050a9
LilCodes <valeibacacheb@gmail.com>
2021-12-13 00:48:01 -0300

    Co-authored-by: Miguel Segovia <Miikkkkk@users.noreply.github.com> Co-authored-by: Benjamin Cayo <becayo@users.noreply.github.com> Co-authored-by: Unifroxt <Unifroxt@users.noreply.github.com> Co-authored-by: Sebastián Torrealba <storrealbac@users.noreply.github.com>

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 from os import name
 from flask import Flask, render_template, request, redirect, url_for
+from flask.globals import session
 from flask_socketio import SocketIO, join_room, leave_room, emit
+
+from flask_session import Session
 
 app=Flask(__name__)
 
@@ -8,6 +11,7 @@ app.debug = True
 app.config['SECRET_KEY'] = 'secret'
 app.config['SESSION_TYPE'] = 'filesystem'
 
+Session(app)
 
 socketio = SocketIO(app, manage_session=False)
 
@@ -23,11 +27,18 @@ def index():
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
+        nm = request.form['nm']
+        session['nick'] = request.form['nm']
+        session['nm'] = nm
+        return redirect(url_for('success',nm = nm))
    else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+        if (session.get('nm') is not None):
+            nm = request.form.get('nm')
+
+            session['nm'] = nm
+            return redirect(url_for('success',nm = nm))
+        else:
+            return redirect(url_for('index'))
 
 @app.route('/main')
 def success():
@@ -43,14 +54,23 @@ def crear1():
     if request.method == "GET":
         name = request.form["name_sala"]
         password = request.form["contrasenia"]
-        return redirect(url_for('tutor', name_sala = name))
-    else:
-        name = request.form.get("name_sala")
-        password = request.form.get("contrasenia")
-        return redirect(url_for('tutor', name_sala = name))
 
-@app.route('/Tutor/<name_sala>')
-def tutor(name_sala):
+        session['nm'] = name
+        session['nm'] = password
+        return redirect(url_for('tutor', session = session))
+    else:
+        if (session.get('nm') is not None):
+            name = request.form.get("name_sala")
+            password = request.form.get("contrasenia")
+
+            session['nm'] = name
+            session['nm'] = password
+            return redirect(url_for('tutor', session = session))
+        else:
+            return redirect(url_for('index'))
+
+@app.route('/Tutor')
+def tutor():
     return render_template('Tutor.html')
 
 #---------------
@@ -62,10 +82,17 @@ def ingresar():
 def ingresar1():
     if request.method == "GET":
         password1 = request.form["contrasena"]
+
+        session['nm'] = password1
         return redirect(url_for('pupilo'))
     else:
-        password1 = request.form.get("contrasena")
-        return redirect(url_for('pupilo'))
+        if (session.get('nm') is not None):
+            password1 = request.form.get("contrasena")
+
+            session['nm'] = password1
+            return redirect(url_for('pupilo'))
+        else:
+            return redirect(url_for('index'))
 
 @app.route('/Pupilo')
 def pupilo():
@@ -79,31 +106,24 @@ def text(message):
 
 #-------------
 #-------------
-'''
+
 @socketio.on('join', namespace='/chat')
 def join(message):
-    room = session.get('room')
-    join_room(room)
-    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
+    #room = session.get('name')
+    #print(room)
+    #join_room(room)
+    print("Restarting web")
+    emit('status', {'msg':  session.get('nick') + ' ha entrado a la sala. ¡Saludos!.'})
 
 
-@socketio.on('text', namespace='/chat')
+@socketio.on('message', namespace='/chat')
 def text(message):
-    room = session.get('room')
-    emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
+    #room = session.get('name')
+    #, room=room
+    emit('message', {'msg': session.get('nick') + ' : ' + message['msg']},  broadcast=True)
 
-
-@socketio.on('left', namespace='/chat')
-def left(message):
-    room = session.get('room')
-    username = session.get('username')
-    leave_room(room)
-    session.clear()
-    emit('status', {'msg': username + ' has left the room.'}, room=room)
-'''
 #-------------
 #---------------
 
 if __name__ == "__main__":
-    #socketio.on(app)
     app.run(port = 3000, debug = True)
